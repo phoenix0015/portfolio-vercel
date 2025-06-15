99fa5f3865fb6b8367fc6b226af49aa2f242d049
phoenix0015 <tadashiharoku@gmail.com>
2025-06-15 10:02:51 -0700

    Added CI/CD pipeline to the sanity backend part

--- backend-sanity/sanity.cli.ts
+++ backend-sanity/sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: '3z4l2vp4',
     dataset: 'production'
   },
+  studioHost: 'tadashiharoku-portfolio',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
--- frontend/lib/sanity.client.js
+++ frontend/lib/sanity.client.js
@@ -6,6 +6,9 @@ export const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
+  withCredentials: true
 })
 
 const builder = imageUrlBuilder(client)
